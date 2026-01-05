da3e9bb45c1f176e39d57c947f51fc1543fdf4fd
Ukasha-hub <ukashajljl@gmail.com>
2026-01-05 16:26:45 +0600

    table lock

--- app/api/v1/endpoints/hourly_ad.py
+++ app/api/v1/endpoints/hourly_ad.py
@@ -5,6 +5,8 @@ from app.db.session import get_db
 from app.models.hourly_ad import HourlyAdSetting
 from app.schemas.hourly_ad import HourlyAdCreate, HourlyAdRead, HourlyAdUpdate
 from app.utils.logger import log_action
+from app.services.storage.db_lock import execute_with_table_lock
+import time
 
 router = APIRouter()
 
@@ -14,7 +16,8 @@ router = APIRouter()
 @router.get("/", response_model=HourlyAdRead)
 def get_hourly_ad_settings(db: Session = Depends(get_db)):
     settings = db.query(HourlyAdSetting).first()
-
+    
+    
     if not settings:
         # return default if nothing stored yet
         return HourlyAdSetting(id=0, hourly_interval="01:00:00")
@@ -27,20 +30,31 @@ def get_hourly_ad_settings(db: Session = Depends(get_db)):
 # -------------------------------------------------
 @router.post("/", response_model=HourlyAdRead)
 def save_hourly_ad_settings(payload: HourlyAdCreate, db: Session = Depends(get_db)):
-    settings = db.query(HourlyAdSetting).first()
+    def operation():
+        settings = db.query(HourlyAdSetting).first()
+        time.sleep(20)  
+        if settings:
+            # UPDATE
+            settings.hourly_interval = payload.hourly_interval
+            db.flush()
+            log_action(
+                db,
+                emp_id="101",
+                action=f"Updated hourly interval to {payload.hourly_interval}"
+            )
+            return settings
 
-    if settings:
-        # UPDATE
-        settings.hourly_interval = payload.hourly_interval
-        db.commit()
-        db.refresh(settings)
-        return settings
+        # CREATE
+        new_settings = HourlyAdSetting(hourly_interval=payload.hourly_interval)
+        db.add(new_settings)
+        db.flush()
+        log_action(db, emp_id="101", action=f"Updated hourly interval to {payload.hourly_interval}")
 
-    # CREATE
-    new_settings = HourlyAdSetting(hourly_interval=payload.hourly_interval)
-    db.add(new_settings)
-    db.commit()
-    db.refresh(new_settings)
-    log_action(db, emp_id="101", action=f"Updated hourly interval to {payload.hourly_interval}")
-
-    return new_settings
+        return new_settings
+     
+    return execute_with_table_lock(
+        db=db,
+        table_name="hourly_ad_settings",
+        operation=operation,
+        success_message="Hourly ad settings saved successfully"
+    )
---
+++ app/services/storage/db_lock.py
@@ -0,0 +1,39 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import OperationalError
+from fastapi import HTTPException
+from sqlalchemy import text
+
+def execute_with_table_lock(
+    db: Session,
+    table_name: str,
+    operation,
+    success_message: str = "Execution successful"
+):
+    try:
+        # Set lock timeout (prevents infinite waiting)
+        #db.execute(text("SET lock_timeout = '5s'"))
+
+        # Lock table
+        db.execute(text("LOCK TABLE hourly_ad_settings IN EXCLUSIVE MODE"))
+
+        # Execute the business logic
+        result = operation()
+
+        # Commit releases the lock
+        db.commit()
+
+        return result
+
+    except OperationalError:
+        db.rollback()
+        raise HTTPException(
+            status_code=409,
+            detail="Table is busy. Please try again."
+        )
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
